67c68fdfc6a073ec44165ec1bf0e9794f4c82921
Vagner Farias <vfarias@gmail.com>
2017-10-26 14:44:13 +0000

    Including TLS configuration for the public endpoints

--- templates/deploy.sh
+++ templates/deploy.sh
@@ -2,5 +2,6 @@
 openstack overcloud deploy --timeout 90  \
                            --templates \
                            --environment-directory /home/stack/templates/environments \
+                            -e /usr/share/openstack-tripleo-heat-templates/environments/tls-endpoints-public-ip.yaml \
                            --ntp-server 2.fedora.pool.ntp.org 
 
